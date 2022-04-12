9e7398b69814d25a1894ee2e5c83ec7955d75d82
jean <leejeanhwa@gmail.com>
2022-04-12 10:19:32 +0800

    comment out settings in playwright.config.js

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,11 +3,11 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
   timeout: 120000,
-  use: {
-    screenshot: 'only-on-failure'
-  },
-  reporter: [
-    ['allure-playwright']
-  ],
+  // use: {
+  //   screenshot: 'only-on-failure'
+  // },
+  // reporter: [
+  //   ['allure-playwright']
+  // ],
 };
 export default config;
