4d7d4ce52aefcb19907fb2124b018296ff871d6c
jean <leejeanhwa@gmail.com>
2022-04-20 12:49:25 +0800

    change to login gmail via cookies by setting a non-expired date

--- pages/setup.ts
+++ pages/setup.ts
@@ -1,19 +1,37 @@
-import { Browser, chromium } from '@playwright/test'
+import { Browser, BrowserContext, chromium } from '@playwright/test'
+import fs from 'fs'
 
 export class Setup {
     private browser: Browser
+    private context: BrowserContext
 
     async initBrowser() {
         this.browser = await chromium.launch({ headless: false })
     }
 
     async initPage() {
-        const context = await this.browser.newContext({
+        this.context = await this.browser.newContext({
             // recordVideo: {
             //     dir: 'videos/',
             //     size: { width: 1080, height: 1920 }
             // }
         })
-        return await context.newPage()
+        return await this.context.newPage()
+    }
+
+    async getContext() {
+        return this.context
+    }
+
+    async setCookieExpireDate(cookie: any) {
+        for (const c of cookie) {
+            // set expires property to now plus extra 3 minutes (180 seconds)
+            c.expires = (Date.now() / 1000) + 180
+        }
+        this.context.addCookies(cookie)
+    }
+
+    async getCookieAndStore(path: string) {
+        fs.writeFileSync(path, JSON.stringify(await this.context.cookies()))
     }
 }
--- tests/test-suite.spec.ts
+++ tests/test-suite.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect, Page } from '@playwright/test'
+import ckSender from '../cookies/sender.json'
+import ckReceiver from '../cookies/receiver.json'
 import data from '../fixtures/data.json'
 import { Login } from '../pages/login'
 import { Setup } from '../pages/setup'
@@ -27,7 +29,10 @@ test.describe('Test Suite', () => {
 
     test('e2e test mail merge', async () => {
         // login to user account and go to file url
-        await login.loginAccount(data.urlFile, data.emailSender.email, data.emailSender.pwd)
+        // await login.loginAccount(data.urlFile, data.emailSender.email, data.emailSender.pwd)
+        // set non-expired cookie
+        await setup.setCookieExpireDate(ckSender[0])
+        await page.goto(data.urlFile)
         // clear spreadsheet data
         await helper.clearSpreadSheetData()
         // enter spreadsheet data
@@ -38,13 +43,23 @@ test.describe('Test Suite', () => {
         let result = await helper.checkMergeStatus('EMAIL_SENT', 0, 5)
         expect(result).toBeTruthy()
 
-        // login to each recipient's email and open latest email in inbox
-        for (const user of data.emailReceivers) {
+        // // login to each recipient's email and open latest email in inbox
+        // for (const user of data.emailReceivers) {
+        //     await setup.initBrowser()
+        //     page = await setup.initPage()
+        //     login = new Login(page)
+        //     email = new Email(page)
+        //     await login.loginAccount(data.urlEmail, user.email, user.pwd)
+        //     await email.clickLatestInbox()
+        // }
+
+        // open latest inbox of each receivers
+        for (const cookie of ckReceiver) {
             await setup.initBrowser()
             page = await setup.initPage()
-            login = new Login(page)
+            await setup.setCookieExpireDate(cookie)
             email = new Email(page)
-            await login.loginAccount(data.urlEmail, user.email, user.pwd)
+            await page.goto(data.urlEmail)
             await email.clickLatestInbox()
         }
 
